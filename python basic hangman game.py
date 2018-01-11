9998534787a1e0ad91221d1f2945754cb6e44c1b
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-11 18:29:32 +0500

    Add files via upload
    this is a Hangman game.. the program give you a random word (not a valid word in English language, its a random word).. and you guess...

---
+++ python basic hangman game.py
@@ -0,0 +1,29 @@
+# Exercise #30 & 31 & 32
+'''
+def pick_word():
+    return random.choice([i.lower() for i in open('/home/hp/Desktop/Exercises from www.practivepython org/exercise_30_related file.txt', 'r').read().split()])
+'''
+def guess_letters():
+    import random
+    ll = int(input('how long word you wish to guess? '))
+    a = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(ll)] 
+    m = a[:]; l = 0; ss = list()
+    d = ['_' for i in range(len(a))]
+    print('Welcome to Hangman!')
+    [print(i, end='') for i in d]; print('\n')
+    while '_' in d and l < ll:
+        if d != m:
+            print(str(ll-l) + ' turn(s) left')
+            b = input('Enter a single latter: '); ss.append(b)
+            if len(b) == 1 and b not in d and ss.count(b)==1:
+                l +=1
+                if len(b) == 1 and b in a:
+                    for i in range(a.count(b)):
+                        d[a.index(b)] = b; a[a.index(b)]= ' '
+                [print(i, end='') for i in d]; print('\n')
+            else: print('you entered this latter before.. plz enter onother latter..\n')
+    if d == m:
+        return 'Congratulation! you guess the word in ' + str(l)+' turns\n[for restart the game type guess_letters()]'
+    else: return 'Game over!... try again\n[for restart the game type guess_letters()]'
+print(guess_letters())
+
