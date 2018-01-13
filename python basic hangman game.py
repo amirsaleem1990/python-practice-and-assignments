e6036fabc09cef46fa784f6a0c0d15bcb9e7b809
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-13 12:41:58 +0500

    Update python basic hangman game.py

--- python basic hangman game.py
+++ python basic hangman game.py
@@ -3,27 +3,32 @@
 def pick_word():
     return random.choice([i.lower() for i in open('/home/hp/Desktop/Exercises from www.practivepython org/exercise_30_related file.txt', 'r').read().split()])
 '''
-def guess_letters():
+def Hangman_game():
     import random
+    print('\nWelcome to Hangman Game!\n')
     ll = int(input('how long word you wish to guess? '))
-    a = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(ll)] 
+    a = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(ll)]
     m = a[:]; l = 0; ss = list()
     d = ['_' for i in range(len(a))]
-    print('Welcome to Hangman!')
     [print(i, end='') for i in d]; print('\n')
     while '_' in d and l < ll:
         if d != m:
             print(str(ll-l) + ' turn(s) left')
-            b = input('Enter a single latter: '); ss.append(b)
+            b = input('Enter a single latter: ').lower(); ss.append(b)
             if len(b) == 1 and b not in d and ss.count(b)==1:
                 l +=1
-                if len(b) == 1 and b in a:
+                if b in a:
                     for i in range(a.count(b)):
                         d[a.index(b)] = b; a[a.index(b)]= ' '
                 [print(i, end='') for i in d]; print('\n')
             else: print('you entered this latter before.. plz enter onother latter..\n')
     if d == m:
-        return 'Congratulation! you guess the word in ' + str(l)+' turns\n[for restart the game type guess_letters()]'
-    else: return 'Game over!... try again\n[for restart the game type guess_letters()]'
-print(guess_letters())
+        return 'Congratulation! you guess the word in ' + str(l)+' turns\n'
+    else: print('Game over!..... try again\n'+'your word is this which you guessed: ', end='')
+    [print(i, end='') for i in m]
+    return ''
 
+print(Hangman_game())
+nn = input('\nAre you want to play this game once more? \n[y/n] ')
+while nn.lower() == 'y': print(Hangman_game())
+if nn.lower() == 'n': print('Take care.. by by')
