0bc9a14211db6bf389407c103bf6a97e33fc2e84
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-13 15:44:30 +0500

    Delete Exercise #7.py

--- Exercise #7.py
+++
@@ -1,9 +0,0 @@
-# Exersize #7
-# http://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
-'''
-Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
-Write one line of Python that takes this list a and makes a new list that has only the even
-elements of this list in it.
-'''
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-print([i for i in a if i % 2 == 0])
