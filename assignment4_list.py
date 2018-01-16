50dc82630645d5b1472f8c8128207e82c4950ff4
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-17 01:56:56 +0500

    updated

--- assignment4_list.py
+++ assignment4_list.py
@@ -1,106 +1,54 @@
-# Basic string exercises
-# Fill in the code for the functions below. main() is already set up
-# to call the functions with a few different inputs,
-# printing 'OK' when each function is correct.
-# The starter code for each function includes a 'return'
-# which is just a placeholder for your code.
-# It's ok if you do not complete all the functions, and there
-# are some additional functions to try in assignment2.py.
+# Additional basic list exercises
+
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+
+def remove_adjacent(nums):
+    return list(set(nums))
 
 
-# A. donuts
-# Given an int count of a number of donuts, return a string
-# of the form 'Number of donuts: <count>', where <count> is the number
-# passed in. However, if the count is 10 or more, then use the word 'many'
-# instead of the actual count.
-# So donuts(5) returns 'Number of donuts: 5'
-# and donuts(23) returns 'Number of donuts: many'
-def donuts(count):
-  if count >9: return 'Number of donuts: many'
-  else: return 'Number of donuts: ' + str(count)
-      
-    
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+def linear_merge(list1, list2):
+    return sorted(list1+list2)
+# Note: the solution above is kind of cute, but unforunately list.pop(0)
+# is not constant time with the standard python list implementation, so
+# the above is not strictly linear time.
+# An alternate approach uses pop(-1) to remove the endmost elements
+# from each list, building a solution list which is backwards.
+# Then use reversed() to put the result back in the correct order. That
+# solution works in linear time, but is more ugly.
 
 
-# B. both_ends
-# Given a string s, return a string made of the first 2
-# and the last 2 chars of the original string,
-# so 'spring' yields 'spng'. However, if the string length
-# is less than 2, return instead the empty string.
-def both_ends(s):
-  if len(s) < 2: return ''
-  else: return s[ :2]+ s[-2: ]
-  
-# C. fix_start
-# Given a string s, return a string
-# where all occurences of its first char have
-# been changed to '*', except do not change
-# the first char itself.
-# e.g. 'babble' yields 'ba**le'
-# Assume that the string is length 1 or more.
-# Hint: s.replace(stra, strb) returns a version of string s
-# where all instances of stra have been replaced by strb.
-def fix_start(s):
-    a = s.replace(s[0], '*')
-    return s[0] + a[1: ]
-
-
-# D. MixUp
-# Given strings a and b, return a single string with a and b separated
-# by a space '<a> <b>', except swap the first 2 chars of each string.
-# e.g.
-#   'mix', pod' -> 'pox mid'
-#   'dog', 'dinner' -> 'dig donner'
-# Assume a and b are length 2 or more.
-
-def mix_up(a, b):
-  return b[ :2] + a[2: ] + ' ' + a[ :2] + b[2: ]
-  
-
-
-# Provided simple test() function used in main() to print
+# Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
   if got == expected:
     prefix = ' OK '
   else:
     prefix = '  X '
-  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
-# Provided main() calls the above functions with interesting inputs,
-# using test() to check if each result is correct or not.
+# Calls the above functions with interesting inputs.
 def main():
-  print ('donuts')
-  # Each line calls donuts, compares its result to the expected for that call.
-  test(donuts(4), 'Number of donuts: 4')
-  test(donuts(9), 'Number of donuts: 9')
-  test(donuts(10), 'Number of donuts: many')
-  test(donuts(99), 'Number of donuts: many')
+  print ('remove_adjacent')
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
 
-  print ()
-  print ('both_ends')
-  test(both_ends('spring'), 'spng')
-  test(both_ends('Hello'), 'Helo')
-  test(both_ends('a'), '')
-  test(both_ends('xyz'), 'xyyz')
-
-  
-  print()
-  print ('fix_start')
-  test(fix_start('babble'), 'ba**le')
-  test(fix_start('aardvark'), 'a*rdv*rk')
-  test(fix_start('google'), 'goo*le')
-  test(fix_start('donut'), 'donut')
-
-  print()
-  print ('mix_up')
-  test(mix_up('mix', 'pod'), 'pox mid')
-  test(mix_up('dog', 'dinner'), 'dig donner')
-  test(mix_up('gnash', 'sport'), 'spash gnort')
-  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+  print ('linear_merge')
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
 
 
-# Standard boilerplate to call the main() function.
 if __name__ == '__main__':
   main()
