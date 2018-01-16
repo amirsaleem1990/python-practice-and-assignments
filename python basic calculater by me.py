dae6d8278764f5b8a5b699f14b647baa551db927
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-16 23:16:09 +0500

    Add files via upload

---
+++ python basic calculater by me.py
@@ -0,0 +1,16 @@
+# basic calculater
+e = True
+while e:
+    a = input('what you want? \n[add]:\t for addition\n[multi]: for multiplication\n[sub]:\t for substraction\n[div]:\t for division\n')
+    if a in 'addmultisubdiv':
+        d = input('[Enter 2 numbuers with space. eg: 43 9]\n')
+        b = int(d[:d.find(' ')])
+        c = int(d[d.find(' ')+1:])
+        if a == 'add': print(str(b)+' + '+str(c)+' = '+ str (b+c))
+        elif a == 'multi': print(str(b)+' * '+str(c)+' = '+ str (b*c))
+        elif a == 'sub': print(str(b)+' - '+str(c)+' = '+ str (b-c))
+        elif a == 'div': print(str(b)+' / '+str(c)+' = '+ str (b/c))
+        else: print('your choose not in our list.. plz choose correctly')
+        f = input('\nAre you want another calculation?\n[y/n] ')
+        if f.lower() == 'y': e = True
+        else: e = False; print('Thank\'s for using our calculator. Take care.. bye bye')
